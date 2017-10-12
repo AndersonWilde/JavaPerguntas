e9b39a902953f84b982887e765591bf07fd920dc
Guy Novaes <guynovaes@yahoo.com>
2017-10-12 00:54:46 -0300

    Segunda versão

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Trabalho feito em Java para a matéria de Programação do segundo semestre da U
 
 Trata-se de um sistema de perguntas em Java.
 
-A versão utilizada é a 1.8.0.
+A versão utilizada do JDK utilizada é a 1.8.0.
+
+Utilizando o banco de dados DB4O na versão db4o-7.2.44.10827-osgi.jar baixado de https://sourceforge.net/projects/db4o/files/latest/download
--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,22 @@
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("teste GIT");
-
+		com.db4o.ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "banco"); 
+		Pergunta p = new Pergunta();
+		p.setPergunta("Qual é a cor do ceu durante o dia?", 1, "azul", "amarelo", "roxo", "vermelho", "verde");
+		//db.store(p);
+		
+		ObjectSet<Pergunta> o = db.queryByExample(p);
+		
+		Pergunta achou = o.next();
+		System.out.println(achou.descricao);
+		System.out.println(achou.resposta2);
+		System.out.println(achou.responde(2));
+		
 	}
-
 }
